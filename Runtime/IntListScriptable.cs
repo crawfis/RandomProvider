8a84411dfaa3b0077c20e3c66ef8223f18856d8a
Roger Crawfis <roger.crawfis@gmail.com>
2024-05-04 08:58:34 -0400

    Initial commit from Unity Starter Project repo.

---
+++ Runtime/IntListScriptable.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace CrawfisSoftware
+{
+    [CreateAssetMenu(fileName = "intList", menuName = "CrawfisSoftware/Create Scriptable: List<int>")]
+    public class IntListScriptable : ScriptableListGeneric<int>
+    {
+    }
+}
---
+++ Runtime/ScriptableListGeneric.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace CrawfisSoftware
+{
+    public class ScriptableListGeneric<T> : ScriptableObject
+    {
+        [SerializeField] public List<T> List;
+    }
+}
